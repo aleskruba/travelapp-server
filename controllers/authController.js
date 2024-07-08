a3f3b6f8d44109c4d04f498934ada75ec199153e
Ales <aleskruba@seznam.cz>
2024-07-08 18:45:14 +0200

    google auth III

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const crypto = require('crypto');
 let redisClient = redis.createClient({
         //host: '127.0.0.1',
     // host: process.env.REDIS_URL ,
-    host:'redis://red-cq0mg1iju9rs73avmd4g',
+    host:'red-cq0mg1iju9rs73avmd4g',
     port: 6379,
 
 });
