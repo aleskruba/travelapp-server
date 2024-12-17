9244f4d69f241071c52c8b0e5ff2084581efd63d
Ales <aleskruba@seznam.cz>
2024-12-17 16:49:39 +0100

    cors

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 const corsOptions = {
   origin: [ 'http://localhost:3000','https://travelapp-itpa.onrender.com'],
   credentials: true,
+  sameSite: 'lax',
+  domain:  '.onrender.com' 
 };
 
 app.use(cors(corsOptions));
